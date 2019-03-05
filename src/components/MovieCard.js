f502fcd002ba4128eee53723247889e2c473c746
jeff-gosselin <jgoz.email@gmail.com>
2019-03-05 13:30:28 -0500

    before installing react-text-truncate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../App.css';
+
 var moment = require('moment');
 
 const MovieCard = ({movie}) => {
@@ -6,21 +8,34 @@ const MovieCard = ({movie}) => {
   const color = vote_average > 7 ? "default" : "secondary"
 
   return (
-    <div className={`uk-card uk-card-${color} uk-card-hover`}>
-      <div className="uk-card-media-top">
-        <img data-src={`http://image.tmdb.org/t/p/w500/${poster_path}`} alt="" uk-img="true"/>
+    <div className={`uk-card uk-card-default`}>
+      <div className="uk-card-media-top uk-inline">
+
+			<div class="uk-inline uk-visible-toggle">
+					<img data-src={`http://image.tmdb.org/t/p/w500/${poster_path}`} alt=""  uk-img="true"/>
+					<div class="uk-overlay-default uk-position-cover uk-invisible-hover c"></div>
+					<div class="uk-overlay uk-position-bottom uk-dark uk-invisible-hover">
+					<div className="uk-card-header">
+						<h3 className="uk-card-title white-text">{title}
+							<span className="release-date uk-text-small"> ({moment(release_date).format("YYYY")})</span>
+						</h3>
+						<p className="uk-text-meta"></p>
+					</div>
+					<div className="uk-card-body white-text">
+						<p>Fan Score: <span className="bold">{vote_average}/10</span></p>
+						<p className="white-text">{overview}</p>
+						<div className="rent-buy-container">
+							<button className="rent-buy-btn">Rent or Buy</button>
+						</div>
+
+					</div>
+				</div>
+					</div>
+			</div>
+
+
       </div>
-      <div className="uk-card-header">
-        <h3 className="uk-card-title">{title}
-          <span className="uk-text-muted uk-text-small"> ({moment(release_date).format("YYYY")})</span>
-        </h3>
-        <p className="uk-text-meta"></p>
-      </div>
-      <div className="uk-card-body">
-        <p>{vote_average}/10</p>
-      <p className="uk-text-truncate">{overview}</p>
-      </div>
-    </div>
+
   )
 }
 
--- src/containers/PopularMoviesContainer.js
+++ src/containers/PopularMoviesContainer.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import apiConfig from '../apiKeys';
+// import apiConfig from '../apiKeys';
 import MovieCard from '../components/MovieCard';
 import Search from '../components/Search';
 import Login from '../components/Login';
 
 // const popularMoviesURL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiConfig.tmdbKey}`
 const popularMoviesURL = "http://localhost:3000/api/v1/movies/popular"
-const genresURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiConfig.tmdbKey}`
+// const genresURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiConfig.tmdbKey}`
 
 class PopularMoviesContainer extends React.Component {
   state = {
-    movies: [],
-    genres: []
+    movies: []
+    // genres: []
   }
 
   componentDidMount = () => {
@@ -23,9 +23,9 @@ class PopularMoviesContainer extends React.Component {
       }, () => console.log(this.state.movies)
     ))
 
-    fetch(genresURL)
-    .then(res => res.json())
-    .then(data => this.setState({ genres: data.genres }))
+    // fetch(genresURL)
+    // .then(res => res.json())
+    // .then(data => this.setState({ genres: data.genres }))
   }
 
   formatMovieCards = () => {
@@ -34,9 +34,9 @@ class PopularMoviesContainer extends React.Component {
     })
   }
 
-  formatAllGenres = () => {
-    return this.state.genres.map(genre => <li>{genre.name}</li> )
-  }
+  // formatAllGenres = () => {
+  //   return this.state.genres.map(genre => <li>{genre.name}</li> )
+  // }
 
   render() {
     return (
@@ -46,9 +46,6 @@ class PopularMoviesContainer extends React.Component {
 
         <div uk-grid="true" uk-height-match="true" className="uk-child-width-1-4 uk-grid-collapse">
           {this.formatMovieCards()}
-        </div>
-        <div>
-          {this.formatAllGenres()}
         </div>
       </div>
     )
